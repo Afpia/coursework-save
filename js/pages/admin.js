6a98eb8798f434c1966b7ce90c19d8beb194b52f
Nikita <nikiprostoff@gmail.com>
2024-06-22 23:39:32 +0500

    Разработка CRUD системы

--- js/pages/admin.js
+++ js/pages/admin.js
@@ -11,4 +11,28 @@ export function admin() {
       all.innerHTML = 'отменить';
     }
   });
+
+  const pencil = document.querySelectorAll('.admin__catalog-pencil');
+  const inputAll = document.querySelectorAll('.admin__catalog-input');
+  const confirm = document.querySelectorAll('.admin__catalog-confirm');
+
+  for (const iterator of pencil) {
+    confirm.forEach(conf => {
+      inputAll.forEach(item => {
+        if (item.hasAttribute('disabled') && iterator.getAttribute('id') == item.getAttribute('id') && iterator.getAttribute('id') == conf.getAttribute('id')) {
+          iterator.addEventListener('click', () => {
+            item.removeAttribute('disabled');
+            iterator.style.display = 'none';
+            conf.style.display = 'block';
+          });
+        } else {
+          conf.addEventListener('click', () => {
+            conf.style.display = 'none';
+            item.setAttribute('disabled', '');
+            iterator.style.display = 'block';
+          });
+        }
+      });
+    });
+  }
 }
